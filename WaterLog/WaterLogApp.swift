eb341c2cd3302f060be40cafbddd6dbbbbc28da0
JavitoRoGo <130165792+JavitoRoGo@users.noreply.github.com>
2026-05-11 11:33:55 +0100

    Initial Commit

---
+++ WaterLog/WaterLogApp.swift
@@ -0,0 +1,17 @@
+//
+//  WaterLogApp.swift
+//  WaterLog
+//
+//  Created by Javier Rodríguez Gómez on 11/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct WaterLogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
