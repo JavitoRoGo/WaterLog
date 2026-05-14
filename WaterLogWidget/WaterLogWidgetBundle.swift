5e00057b97431428d3999e326d9ce3d137feba78
JavitoRoGo <130165792+JavitoRoGo@users.noreply.github.com>
2026-05-14 12:06:24 +0100

    Creación de widget en blanco. Actualización a Swift 6 y Strict concurrency

---
+++ WaterLogWidget/WaterLogWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  WaterLogWidgetBundle.swift
+//  WaterLogWidget
+//
+//  Created by Javier Rodríguez Gómez on 14/05/2026.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct WaterLogWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        WaterLogWidget()
+    }
+}
